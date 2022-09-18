90d12dd4190c6dd7ddb8933ac037b2d446e32c0d
aditipolkam <aditipolkam@gmail.com>
2022-09-18 19:09:35 +0530

    [CONNECT] wallet

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,49 @@ const TWITTER_HANDLE = 'aditipolkam';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState('');
 
-  const checkIfWalletIsConnected = () => {
-    const { ethereum } = window;
-    if (!ethereum) {
-      console.log("Make sure you have metamask!")
-      return;
-    }
-    else {
-      console.log("we have the ethereum object.", ethereum)
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
 
   const renderNotConnectedContainer = () => (
     <div className='connect-wallet-container'>
       <img src='https://media.giphy.com/media/xUA7b0Klw8Wfor7FWo/giphy.gif' alt="Buidl Gif" />
-      <button className="cta-button connect-wallet-button">
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
         Connect Wallet
       </button>
     </div>
@@ -45,7 +72,7 @@ function App() {
           </header>
         </div>
 
-        {renderNotConnectedContainer()}
+        {!currentAccount && renderNotConnectedContainer()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
