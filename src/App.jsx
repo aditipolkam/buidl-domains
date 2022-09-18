1defbef658a54cb1edb7ede2cd03e176560b4b10
aditipolkam <aditipolkam@gmail.com>
2022-09-18 18:55:45 +0530

    [CHECK] ethereum object availability

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = 'aditipolkam';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+function App() {
+
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have metamask!")
+      return;
+    }
+    else {
+      console.log("we have the ethereum object.", ethereum)
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <div className='connect-wallet-container'>
+      <img src='https://media.giphy.com/media/xUA7b0Klw8Wfor7FWo/giphy.gif' alt="Buidl Gif" />
+      <button className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+  )
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  })
+
+  return (
+    <div className="App">
+      <div className="container">
+
+        <div className="header-container">
+          <header>
+            <div className="left">
+              <p className="title">⚙ Buidl Name Service</p>
+              <p className="subtitle">Your immortal API on the blockchain!</p>
+            </div>
+          </header>
+        </div>
+
+        {renderNotConnectedContainer()}
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
